cf970c712e556a33a1bb7365e24c4013760fcbb0
Matt K <mjkerster@gmail.com>
2016-07-22 14:14:14 -0400

    second commit

--- app/app.js
+++ app/app.js
@@ -8,23 +8,67 @@
 		var cosmotleMain = {
 			templateUrl: 'app/main.html',
 			controller: 'CosmotleCtrl',
+			controllerAs: 'c',
 			resolve: {
 				cosmotleData: ['CosmotleServices', function(CosmotleServices){
 					return CosmotleServices();
 				}]
 			}
 		}
+
+		var getCredit = {
+			templateUrl: 'app/getCredit.html',
+			controller: 'GetCreditCtrl',
+			controllerAs: 'g'
+		}
+
+		$routeProvider.when('/getcredit', getCredit);
 		$routeProvider.otherwise(cosmotleMain)
 		
 	}])
 
-	app.controller('CosmotleCtrl', ['$scope', 'cosmotleData', function($scope, cosmotleData){
+	app.controller('CosmotleCtrl', ['cosmotleData', '$location', function(cosmotleData, $location){
+		var c = this;
 
-		console.log(cosmotleData);
-		$scope.hello = "Hello World!!!!"
+		c.free = cosmotleData[0].data;
+		c.expense = cosmotleData[1].data;
+		c.attendence = cosmotleData[2].data;
+		c.totalCount;
 
-		
+		c.toggleFree = false;
+		c.toggleExpense = true;
+		c.toggleAttendence = false;
+
+		c.title = "Cosmotle"
+
+		c.toggleStats = function(type){
+			if(type === 'expense'){
+				c.toggleExpense = (c.toggleExpense === true) ? false : true;
+			}
+			else if(type === 'free'){
+				c.toggleFree = (c.toggleFree === true) ? false : true;
+			}
+			else if(type === 'attendence'){
+				c.toggleAttendence = (c.toggleAttendence === true) ? false : true;
+			}
+		}
+
+		c.add = function(){
+			$location.path('/getcredit');
+		}
+
 	}]);
+
+	app.controller('GetCreditCtrl', ['$location', function($location){
+		var g = this;
+
+		g.user;
+		g.creditType;
+
+		g.back = function(){
+			$location.path('/');
+		}
+	}])
 
 	app.factory('CosmotleServices', ['$http', '$q', function($http, $q){
 		return function cosmotleServices(){
