ce34f504cceea565d4081cb703bdf74e27060dd2
Matt K <mjkerster@gmail.com>
2016-07-25 09:53:14 -0400

    Server done but need to reduce redundant code
    Plenty of room for improvement with this first release!

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@
 			controllerAs: 'c',
 			resolve: {
 				cosmotleData: ['CosmotleServices', function(CosmotleServices){
-					return CosmotleServices();
+					return CosmotleServices.getCosmotleStats();
 				}]
 			}
 		}
@@ -59,32 +59,80 @@
 
 	}]);
 
-	app.controller('GetCreditCtrl', ['$location', function($location){
+	app.controller('GetCreditCtrl', ['$location', 'CosmotleServices', function($location, CosmotleServices){
 		var g = this;
 
 		g.user;
 		g.creditType;
 
-		g.back = function(){
+		g.back = function(type){
+			console.log('User: '+ g.user + ' Type: '+ type);
+			CosmotleServices.postCosmotleStats(g.user, type);
 			$location.path('/');
 		}
 	}])
 
 	app.factory('CosmotleServices', ['$http', '$q', function($http, $q){
-		return function cosmotleServices(){
-				var free, expense, attendence, defer;
 
-				defer = $q.defer();
-				free = $http.get('/free');
-				expense = $http.get('/expense');
-				attendence = $http.get('/attendence');
+		var holdStats;
+		return {
+			getCosmotleStats: getCosmotleStats,
+			postCosmotleStats: postCosmotleStats
+		}
 
-				$q.all([free,expense,attendence]).then(function (response) {
-					defer.resolve(response);
-				});
+		function getCosmotleStats(){
+			var free, expense, attendence, defer;
 
-				return defer.promise;
-		};
+			defer = $q.defer();
+			free = $http.get('/free');
+			expense = $http.get('/expense');
+			attendence = $http.get('/attendence');
+
+			$q.all([free,expense,attendence]).then(function (response) {
+				holdStats = response;
+				defer.resolve(response);
+			});
+
+			return defer.promise;	
+		}
+
+		function postCosmotleStats(name, type){
+			var freeObj, expenseObj, attendenceObj, sendId;
+
+			if(type === 'free'){
+				_sendObj(type, name, holdStats[0].data);
+				_sendObj('attendence', name, holdStats[2].data);
+
+			}
+			else if(type === 'expense'){
+				_sendObj(type, name, holdStats[1].data);
+				_sendObj('attendence', name, holdStats[2].data);
+			}
+			else if(type === 'attendence'){
+				_sendObj(type, name, holdStats[2].data);
+			}
+
+
+
+			function _sendObj(type, name, obj){
+				var sendObj = {
+					_id:{
+						$oid: ''
+					},
+					name: name,
+					count: ''
+				}
+				for(var i=0; i< obj.length; i++){
+					if(obj[i].name === name){
+						sendObj._id.$oid = obj[i]._id.$oid;
+						sendObj.count = (Number(obj[i].count)+1).toString();
+						$http.put('/'+type+'/'+obj[i]._id.$oid, sendObj);
+					}
+				}
+			}
+			
+		}
+		
 	}]);
 
 })();
--- server.js
+++ server.js
@@ -79,3 +79,102 @@ app.get('/attendence', function(req, res){
 		});
 	});
 });
+
+app.put('/free/:id', function(req, res){
+	var resData;
+	var post_data = JSON.stringify(req.body);
+	var postOptions = {
+		host: 'api.mongolab.com',
+		path: '/api/1/databases/heroku_lphx2t8m/collections/free/'+req.params.id+'?apiKey=NgGXg3kUA9V4eh_fWe-ponEZCk7EINv2',
+		method: 'PUT',
+		headers: {
+		  'Content-Type': 'application/json'
+		}
+	}
+
+	var request = https.request(postOptions, function(response){
+		response.on('data', function(data){
+			console.log('PUT Cosmotle STATUS: '+ response.statusCode);
+			if(!resData){
+				resData = data;
+			}
+			else{
+				resData += data;
+			}
+		});
+		response.on('end', function(){
+		    res.send(resData);
+		});
+	});
+	request.on('error', function(e){
+		console.log('problem with request: ' +e.message);
+	});
+	request.write(post_data);
+	request.end();
+});
+
+app.put('/expense/:id', function(req, res){
+	var resData;
+	var post_data = JSON.stringify(req.body);
+	var postOptions = {
+		host: 'api.mongolab.com',
+		path: '/api/1/databases/heroku_lphx2t8m/collections/expense/'+req.params.id+'?apiKey=NgGXg3kUA9V4eh_fWe-ponEZCk7EINv2',
+		method: 'PUT',
+		headers: {
+		  'Content-Type': 'application/json'
+		}
+	}
+
+	var request = https.request(postOptions, function(response){
+		response.on('data', function(data){
+			console.log('PUT Cosmotle STATUS: '+ response.statusCode);
+			if(!resData){
+				resData = data;
+			}
+			else{
+				resData += data;
+			}
+		});
+		response.on('end', function(){
+		    res.send(resData);
+		});
+	});
+	request.on('error', function(e){
+		console.log('problem with request: ' +e.message);
+	});
+	request.write(post_data);
+	request.end();
+});
+
+app.put('/attendence/:id', function(req, res){
+	var resData;
+	var post_data = JSON.stringify(req.body);
+	var postOptions = {
+		host: 'api.mongolab.com',
+		path: '/api/1/databases/heroku_lphx2t8m/collections/attendence/'+req.params.id+'?apiKey=NgGXg3kUA9V4eh_fWe-ponEZCk7EINv2',
+		method: 'PUT',
+		headers: {
+		  'Content-Type': 'application/json'
+		}
+	}
+
+	var request = https.request(postOptions, function(response){
+		response.on('data', function(data){
+			console.log('PUT Cosmotle STATUS: '+ response.statusCode);
+			if(!resData){
+				resData = data;
+			}
+			else{
+				resData += data;
+			}
+		});
+		response.on('end', function(){
+		    res.send(resData);
+		});
+	});
+	request.on('error', function(e){
+		console.log('problem with request: ' +e.message);
+	});
+	request.write(post_data);
+	request.end();
+});
