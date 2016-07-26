18df4755a360047ae3c22b3f03dc07a603c36f56
Matt K <mjkerster@gmail.com>
2016-07-26 15:12:23 -0400

    disabled SW - updated styles

--- app/app.js
+++ app/app.js
@@ -342,24 +342,24 @@
 
 })();
 
-var initializeServiceWorker = function(){
+// var initializeServiceWorker = function(){
 
-	if ('serviceWorker' in navigator) {
-	  navigator.serviceWorker.register('/serviceworker.js', { scope: '/' }).then(function(reg) {
-	    if(reg.installing) {
-	      console.log('Service worker installing');
-	    } else if(reg.waiting) {
-	      console.log('Service worker installed');
-	    } else if(reg.active) {
-	      console.log('Service worker active');
-	    }
+// 	if ('serviceWorker' in navigator) {
+// 	  navigator.serviceWorker.register('/serviceworker.js', { scope: '/' }).then(function(reg) {
+// 	    if(reg.installing) {
+// 	      console.log('Service worker installing');
+// 	    } else if(reg.waiting) {
+// 	      console.log('Service worker installed');
+// 	    } else if(reg.active) {
+// 	      console.log('Service worker active');
+// 	    }
 	    
-	  }).catch(function(error) {
-	    // registration failed
-	    console.log('Registration failed with ' + error);
-	  });
-	}
-}();
+// 	  }).catch(function(error) {
+// 	    // registration failed
+// 	    console.log('Registration failed with ' + error);
+// 	  });
+// 	}
+// }();
 
 
 function getCalendarDateRange(){
