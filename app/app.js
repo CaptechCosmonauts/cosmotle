c580aa9f1be46be8012409b52fdafacd0cdfc136
Matt K <mjkerster@gmail.com>
2016-08-02 09:14:49 -0400

    initial commit for push notifications

--- app/app.js
+++ app/app.js
@@ -359,24 +359,22 @@
 
 })();
 
-// var initializeServiceWorker = function(){
+var initializeServiceWorker = function(){
 
-// 	if ('serviceWorker' in navigator) {
-// 	  navigator.serviceWorker.register('/serviceworker.js', { scope: '/' }).then(function(reg) {
-// 	    if(reg.installing) {
-// 	      console.log('Service worker installing');
-// 	    } else if(reg.waiting) {
-// 	      console.log('Service worker installed');
-// 	    } else if(reg.active) {
-// 	      console.log('Service worker active');
-// 	    }
-	    
-// 	  }).catch(function(error) {
-// 	    // registration failed
-// 	    console.log('Registration failed with ' + error);
-// 	  });
-// 	}
-// }();
+	if ('serviceWorker' in navigator) {
+	  navigator.serviceWorker.register('/serviceworker.js', { scope: '/' }).then(function(reg) {
+			console.log('REGISTERED SW:', reg);
+			reg.pushManager.subscribe({
+				userVisibleOnly: true
+			}).then(function(sub){
+				console.log('endpoint:', sub.endpoint);
+			});
+	  }).catch(function(error) {
+	    // registration failed
+	    console.log('Registration failed with ' + error);
+	  });
+	}
+}();
 
 var CalendarUtils = function(){
 	'use strict';
--- serviceworker.js
+++ serviceworker.js
@@ -1,33 +1,49 @@
-this.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('cosmotleCacheV2').then(function(cache) {
-      return cache.addAll([
-        '/',  
-        '/index.html',
-        '/app/styles/cosmonauts.jpeg',
-        '/app/styles/cosmotle.css',
-        '/app/styles/error-triangle.svg',
-        '/app/styles/tortilla-yellow.svg',
-        '/app/styles/skeleton.css',
-        '/node_modules/angular/angular.min.js',
-        '/node_modules/angular-route/angular-route.min.js',
-        '/node_modules/angular-cookies/angular-cookies.min.js',
+// this.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open('cosmotleCacheV2').then(function(cache) {
+//       return cache.addAll([
+//         '/',  
+//         '/index.html',
+//         '/app/styles/cosmonauts.jpeg',
+//         '/app/styles/cosmotle.css',
+//         '/app/styles/error-triangle.svg',
+//         '/app/styles/tortilla-yellow.svg',
+//         '/app/styles/skeleton.css',
+//         '/node_modules/angular/angular.min.js',
+//         '/node_modules/angular-route/angular-route.min.js',
+//         '/node_modules/angular-cookies/angular-cookies.min.js',
 
-      ]);
-    })
-  );
-});
+//       ]);
+//     })
+//   );
+// });
 
-this.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response){
-      if(response)
-        return response;
+// this.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(response){
+//       if(response)
+//         return response;
 
-      return fetch(event.request).then(function(response){
-        return response;
-      });
-    })
-  );
+//       return fetch(event.request).then(function(response){
+//         return response;
+//       });
+//     })
+//   );
   
+// });
+
+console.log('Started ', this);
+
+this.addEventListener('install', function(event){
+  this.skipWaiting();
+
+  console.log('SW Install Event', event);
 });
+
+this.addEventListener('activate', function(event){
+  console.log('SW Activated Event', event);
+});
+
+this.addEventListener('push', function(event){
+  console.log('SW Push Event', event);
+})
